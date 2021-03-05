8d2c17cc530b20ce790053555a27f3f7d662ddf1
asmit nepali <mail2asmitnepali@gmail.com>
2021-03-05 15:47:11 +0545

    [IMP] VueJs: Methods.

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 new Vue({
 	el:'#vue-app',
 	data:{
-		name:'Asmit Nepali'
+		name:'Asmit Nepali',
+		job:'Developer',
+	},
+	methods:{
+		greet:function(time){
+			this.name
+			return'Good '+time +' '+ this.name;
+		}
 	}
 });
