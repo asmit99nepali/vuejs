4d870e1d1f4e10350aa504ace78eff6f4d064ef1
asmit nepali <mail2asmitnepali@gmail.com>
2021-03-14 22:07:35 +0545

    [IMP] VueJs: Slot simple example.

---
+++ form-helper/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <form-helper>
+      <h2>{{ title }}</h2>
+      <p>I'm the paragraph title</p>
+   </form-helper>
+  </div>
+</template>
+
+<script>
+import FormHelper from './components/FormHelper';
+
+export default{
+  components:{
+    'form-helper': FormHelper
+  },
+  data(){
+    return{
+      title:"I'm a dynamic slot title",
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
---
+++ form-helper/src/components/FormHelper.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- <slot name="title"></slot> -->
+    <h1>Hello Form Helper</h1>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default{
+    components:{
+
+    },
+    data(){
+      return{
+         
+      }
+    },
+    methods:{
+
+    }
+  }
+</script>
+<style type="text/css" scoped>
+  h1{
+    color:red;
+  }
+</style>
