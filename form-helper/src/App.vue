a1cdc08351c199458a9b7239deee92a3bf908b00
asmit nepali <mail2asmitnepali@gmail.com>
2021-03-15 08:43:46 +0545

    [FIX] VueJs: Slots Probel Solved.
    add  "plugin:vue/vue3-essential","plugin:vue/vue3-strongly-recommended",
    "plugin:vue/vue3-recommended", on package.json file.

--- form-helper/src/App.vue
+++ form-helper/src/App.vue
@@ -1,24 +1,43 @@
 <template>
-  <div>
-  </div>
+    <div>
+        <form-helper>
+            <div slot="form-header">
+                <h3>This is the title of a form</h3>
+                <p>This is some info about the form</p>
+            </div>
+            <div slot="form-fields">
+                <input type="text" placeholder="name" required />
+                <input type="password" placeholder="password" required />
+            </div>
+            <div slot="form-controls">
+                <button v-on:click="handleSubmit">Submit</button>
+            </div>
+        </form-helper>
+    </div>
 </template>
 
 <script>
-import FormOne from './components/FormOne';
-import FormTwo from './components/FormTwo';
-
-export default{
-  components:{
-    'form-one': FormOne,
-    'form-two': FormTwo,
-  },
-  data(){
-    return{
-      title:"I'm a dynamic slot title",
+// Imports
+import FormHelper from './components/FormHelper.vue'
+export default {
+    components: {
+        'form-helper': FormHelper
+    },
+    data () {
+        return {
+        }
+    },
+    methods: {
+        handleSubmit: function(){
+            alert('thanks for submitting');
+        }
     }
-  },
-  methods:{
-
-  }
 }
 </script>
+
+<style>
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+}
+</style>
